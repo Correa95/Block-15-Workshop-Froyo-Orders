8d2cc48e7c75fa2ab122a23826c60a48fc79f7d3
Correa95 <mathewcorrea19@gmail.com>
2024-08-02 10:29:46 -0400

    submitting my block15 workshop

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const userInputOrder = prompt(
 );
 
 const userInfor = userInputOrder.split(",");
-// console.table(userInfor);
+console.table(userInfor);
 
 function checkingForFlavor(userInfor) {
   const flavorObj = {};
-  for (const items in userInfor.length) {
-    flavorObj[items] = (flavorObj[items] || 0) + 1;
+
+  for (let i = 0; i < userInfor.length; i++) {
+    flavorObj[userInfor[i]] = (flavorObj[userInfor[i]] || 0) + 1;
   }
   return flavorObj;
 }
